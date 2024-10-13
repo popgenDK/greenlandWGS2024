36c9decacac961475d74ba26528ed7b7c7726b94
SHIXU <shixu.he@bio.ku.dk>
2024-10-13 22:32:15 +0200

    Update NGSremix.sh

--- NGSremix/NGSremix.sh
+++ NGSremix/NGSremix.sh
@@ -15,13 +15,13 @@ plink --allow-extra-chr --bfile ${RAW_PLINK_PREFIX} --chr 1-22,xy --indep-pairwi
 PLINK_PREFIX=input.plink.maf05.geno05.ldpruned
 plink --allow-extra-chr --bfile ${RAW_PLINK_PREFIX} --extract tmp.plink.prune.in --make-bed --out ${PLINK_PREFIX}
 
-### Step2 run admixture with K=2 to obtain population allele frequency and individual ancestry proportion. As described in NGSremix README
-### https://github.com/KHanghoj/NGSremix?tab=readme-ov-file#called-genotypes
+### Step2 run admixture with K=2 to obtain population allele frequency and individual ancestry proportion. As described in relatedAdmix README
+### https://github.com/aalbrechtsen/relateAdmix?tab=readme-ov-file#c-from-commandline
 
 admixture -j60 ${PLINK_PREFIX}.bed 2 > ${PLINK_PREFIX}.admixture.log
 
 ### Step3 run NGSremix to estimate relatedness result
 ### This step will generate the "ngsremix.res" file, which contains the results required for downstream full sibling or parent-offspring analysis
 
-NGSremix -plink ${PLINK_PREFIX} -f ${PLINK_PREFIX}.2.P -q ${PLINK_PREFIX}.2.Q -P 20 -o ${PLINK_PREFIX}.kin
+relatedAdmix -plink ${PLINK_PREFIX} -f ${PLINK_PREFIX}.2.P -q ${PLINK_PREFIX}.2.Q -P 20 -o ${PLINK_PREFIX}.kin
 ls -lh ngsremix.res
